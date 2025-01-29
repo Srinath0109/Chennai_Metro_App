1d2018e7c985cacdc250101cf0f2193e7a642bb9
Srinath0109 <162076816+Srinath0109@users.noreply.github.com>
2025-01-29 21:18:40 +0530

    models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+METRO_STATIONS = [
+    "Washermanpet", "Mannadi", "High Court", "Central", "Egmore",
+    "Nehru Park", "Kilpauk", "Pachaiyappa's", "Shenoy Nagar",
+    "Anna Nagar East", "Anna Nagar Tower", "Thirumangalam"
+]
+
+TRAIN_SCHEDULES = [
+    {"station": "Central", "arrival": "10:15 AM", "departure": "10:20 AM"},
+    {"station": "Egmore", "arrival": "10:25 AM", "departure": "10:30 AM"},
+    {"station": "Nehru Park", "arrival": "10:35 AM", "departure": "10:40 AM"},
+]
